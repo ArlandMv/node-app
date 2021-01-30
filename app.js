ae275cf4ed1824ef37d0c9e8ed82f07b1c915d3d
Arland Michelena <arlandmvillegas@gmail.com>
2021-01-30 11:52:34 -0300

    setup /public for future static resources, added /css/styles.css

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv'); //env_var
 const morgan = require('morgan'); //logs
 const exphbs = require('express-handlebars'); //template engine
 
-
 const connectDB = require('./config/db');
-const { connection } = require('mongoose');
 
 //Load config
 dotenv.config({path: './config/config.env' });
-
-//Handlebars
-/*
-app.engine('.hbs', exphbs({ 
-    defaultLayout: 'main', extname: '.hbs'
-}));
-app.set('view engine', '.hbs');*/
 
 connectDB();
 
 const app = express();
 
-if(process.ENV_ENV === 'development'){
-    app.use(morgan('dev'))
+//Logging
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
 }
 
-app.get("/", (req, res) => res.send("Server is up and running"));
+//Handlebars
+app.engine('.hbs', exphbs({ 
+    defaultLayout: 'main', extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+//Static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+//Routes
+app.use('/', require('./routes/index'));
 
 const PORT = (process.env.PORT || 5000 );
 
